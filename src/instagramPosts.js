761fb4bd22c9c8228fb000d40ac02970914a58fa
Kumar Vanshaj <vanshaj@thewordofmouth.tech>
2026-05-19 12:03:10 +0530

    Replace old Instagram reel URLs with post URLs
    Updated Instagram post URLs to new format.

--- src/instagramPosts.js
+++ src/instagramPosts.js
@@ -3,14 +3,14 @@
  *  Format: https://www.instagram.com/p/<POST_ID>/
  */
 export const INSTAGRAM_POST_URLS = [
-  'https://www.instagram.com/reel/DRyhYibk14y/',
-  'https://www.instagram.com/reel/DKwEUUIT4MX/',
-  'https://www.instagram.com/reel/DYRZOGEJ87e/',
-  'https://www.instagram.com/reel/DYMwcgUJEb-/',
-  'https://www.instagram.com/reel/DYJUhAFOAIo/',
-  'https://www.instagram.com/reel/DYHIWHkNNvs/',
-  'https://www.instagram.com/reel/DYEpa41hcXC/',
-  'https://www.instagram.com/reel/DYB0MFRhviE/',
-  'https://www.instagram.com/reel/DX6xfTBNovD/',
-  'https://www.instagram.com/reel/DX6QTfPtDVd/',
+  'https://www.instagram.com/p/DXmGpUtSox_/',
+  'https://www.instagram.com/p/DWESwo3CUTC/',
+  'https://www.instagram.com/p/DW1W5NODEVZ/',
+  'https://www.instagram.com/p/DVvovI1jBpm/',
+  'https://www.instagram.com/p/DVlpZgoCTR-/',
+  'https://www.instagram.com/p/DVQ8QDdCdhq/',
+  'https://www.instagram.com/p/DU8Ow0TCSvb/',
+  'https://www.instagram.com/p/DPB0iQjiY_O/?img_index=1',
+  'https://www.instagram.com/p/DNgI-ZuoP3G/?img_index=1',
+  'https://www.instagram.com/p/DXMlrQ7ifAa/?img_index=1',
 ];
